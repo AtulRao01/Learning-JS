6637d53bc10ae9cae9bce11add31974f369d5593
AtulRao01 <atulrao40@gmail.com>
2022-05-10 03:02:19 +0530

    Starting JS

---
+++ Index.js
@@ -0,0 +1,16 @@
+var a;
+a= 10;
+console.log(typeof a);
+console.log(a);
+
+a= "hello";
+console.log(typeof a);
+console.log(a);
+
+a= true;
+console.log(typeof a);
+console.log(a);
+
+a= null;
+console.log(typeof a);
+console.log(a);
